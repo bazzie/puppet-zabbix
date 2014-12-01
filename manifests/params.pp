4399db6daa1cea09fbd7f30a1e1ed9442a83919a
Bas Wildemann <bas@wildemann.nl>
2014-12-01 13:38:52 +0100

    Database parameters

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,5 +3,11 @@ class zabbix::params {
   $db_type        = 'postgresql'
   $manage_repo    = true 
   $zabbix_version = '2.4'
+  $dbhost         = 'localhost'
+  $dbname         = 'zabbix'
+  $dbschema       = ''
+  $dbuser         = 'zabbix'
+  $dbpassword     = 'zabbix'
+  $dbport         = '5432'
 
 }
