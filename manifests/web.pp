9c06a532d79315e672dd7a271d7f9f210d8ad2de
in468wil <in468wil@DUO-B1PSTD-0105.duo.local>
2014-12-03 10:10:46 +0100

    zabbix_server and zabbix_web separated in it's own classses

---
+++ manifests/web.pp
@@ -0,0 +1,124 @@
+class zabbix::web (
+  
+  $db_type          = $zabbix::params::db_type,
+  $zabbix_url       = $zabbix::params::zabbix_url,
+  
+  
+  
+  
+) inherits zabbix::params {
+  
+  
+  case $db_type {
+    'postgresql': {
+      $db = 'pgsql'
+    }
+    'mysql': {
+      $db = 'mysql'
+    }
+    default: {
+      fail('$db_type is not recognized as a database type for Zabbix.')
+    }
+  }
+  
+  package { "zabbix-web-${db}":
+    ensure  => present,
+    require => Package["zabbix-server-${db}"],
+    before  => [
+      File['/etc/zabbix/web/zabbix.conf.php'],
+        Package['zabbix-web']
+      ],
+  }
+      
+  package { 'zabbix-web':
+    ensure => present,
+  }
+  
+  
+  file { '/etc/zabbix/web/zabbix.conf.php':
+    ensure  => present,
+    owner   => 'zabbix',
+    group   => 'zabbix',
+    mode    => '0644',
+    notify  => Service['zabbix-server'],
+    replace => true,
+    content => template('zabbix/zabbix.conf.php.erb'),
+  }
+  
+  if $manage_vhost {
+    include apache
+    
+    class { 'apache::mod::php': 
+      content => '
+        AddHandler php5-script .php
+        AddType text/html .php',
+        path  => "${::apache::params::lib_path}/libphp5.so",
+    }
+    
+    if $apache_use_ssl {
+      $apache_listen_port = '443'
+ 
+      apache::vhost { "${zabbix_url}_nonssl":
+        docroot        => '/usr/share/zabbix',
+        manage_docroot => false,
+        port           => '80',
+        servername     => $zabbix_url,
+        ssl            => false,
+        rewrites       => [
+          {
+            comment      => 'redirect all to https',
+            rewrite_cond => ['%{SERVER_PORT} !^443$'],
+            rewrite_rule => ["^/(.+)$ https://${zabbix_url}/\$1 [L,R]"],
+          }
+        ],
+      }
+    } else {
+      $apache_listen_port = '80'
+    }
+
+    apache::vhost { $zabbix_url:
+      docroot         => '/usr/share/zabbix',
+      port            => $apache_listen_port,
+      directories     => [
+        { path     => '/usr/share/zabbix',
+          provider => 'directory',
+          allow    => 'from all',
+          order    => 'Allow,Deny',
+        },
+        { path     => '/usr/share/zabbix/conf',
+          provider => 'directory',
+          deny     => 'from all',
+          order    => 'Deny,Allow',
+        },
+        { path     => '/usr/share/zabbix/api',
+          provider => 'directory',
+          deny     => 'from all',
+          order    => 'Deny,Allow',
+        },
+        { path     => '/usr/share/zabbix/include',
+          provider => 'directory',
+          deny     => 'from all',
+          order    => 'Deny,Allow',
+        },
+        { path     => '/usr/share/zabbix/include/classes',
+          provider => 'directory',
+          deny     => 'from all',
+          order    => 'Deny,Allow',
+        },
+      ],
+      custom_fragment => "  php_value max_execution_time 300
+        php_value memory_limit 128M
+        php_value post_max_size 16M
+        php_value upload_max_filesize 2M
+        php_value max_input_time 300
+        php_value date.timezone ${zabbix_timezone}",
+        rewrites   => [ { rewrite_rule    => ['^$ /index.php [L]'] } ],
+        ssl        => $apache_use_ssl,
+        ssl_cert   => $apache_ssl_cert,
+        ssl_key    => $apache_ssl_key,
+        ssl_cipher => $apache_ssl_cipher,
+        ssl_chain  => $apache_ssl_chain,
+    }
+  }
+  
+}
