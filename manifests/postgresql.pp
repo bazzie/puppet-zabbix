3c08407f810635941f36a3ce2b488218a7913f5e
Bas Wildemann <bas@wildemann.nl>
2014-12-16 17:41:06 +0100

    deleted listen_address

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -5,7 +5,6 @@ class zabbix::postgresql(
   $db_pass              = 'zabbix',
   $cidr                 = '192.168.1.0/24',
   $method               = 'trust',
-  $listen_address       = '*',
   $postgres_password    = '',  
   $type                 = 'host',
 
@@ -14,7 +13,7 @@ class zabbix::postgresql(
   class  { 'postgresql::server': 
     ip_mask_deny_postgres_user => '0.0.0.0/32',
     ip_mask_allow_all_users    => '0.0.0.0/0',
-    listen_addresses           => $listen_address,
+    listen_addresses           => '*',
     ipv4acls                   => ['$type $db_name $db_user $cidr $method'],
     postgres_password          => '$postgres_password',
   }
