94ea41e0dc843397ace0dce8b02a77ce63546ab4
Bas Wildemann <bas@wildemann.nl>
2014-12-16 21:48:27 +0100

    test

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -14,7 +14,7 @@ class  { 'postgresql::server':
   ip_mask_deny_postgres_user => '0.0.0.0/32',
   ip_mask_allow_all_users    => '0.0.0.0/0',
   listen_addresses           => "*",
-  ipv4acls                   => ['$type $db_name $db_user $cidr $method'],
+  ipv4acls                   => ["$type $db_name $db_user $cidr $method"],
   postgres_password          => '$postgres_password',
 }
 
