19d8ac85778612b51d53930dec7b2a7b283aed2a
Bas Wildemann <bas@wildemann.nl>
2014-12-16 13:29:20 +0100

    test

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -14,8 +14,8 @@ class zabbix::postgresql(
   class { 'postgresql::server':
     ip_mask_deny_postgres_user => '0.0.0.0/32',
     ip_mask_allow_all_users    => '0.0.0.0/0',
-    listen_addresses           => '*',
-    ipv4acls                   => ["$type $db_name $db_user $cidr $method"],
+    listen_addresses           => $listen_address,
+    ipv4acls                   => [$type $db_name $db_user $cidr $method],
     postgres_password          => '$postgres_password',
   }
 
