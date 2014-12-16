2e83a7442ae44d7929ea83a1b9191bf18deaf29e
Bas Wildemann <bas@wildemann.nl>
2014-12-16 14:05:52 +0100

    jaja

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -11,7 +11,7 @@ class zabbix::postgresql(
 
 ){
   
-  class  {'::postgresql::server': 
+  class  {'postgresql::server': 
     ip_mask_deny_postgres_user => '0.0.0.0/32',
     ip_mask_allow_all_users    => '0.0.0.0/0',
     listen_addresses           => $listen_address,
